3532c525fc6cd23521fde014f80681d3477840be
Mr-Maccoy <134474843+Mr-Maccoy@users.noreply.github.com>
2025-01-08 21:47:04 -0600

    User Session Data & Logout

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -160,7 +160,7 @@ class Users extends Controller{
                 $loggedInUser = $this->userModel->login($data['email'], $data['password']);
                 if($loggedInUser){
                     // Create Session
-                    die('Success');
+                    $this->createUserSession($loggedInUser);
                 }else {
                     $data['password_err'] = 'Password incorrect';
                     $this->view('users/login',$data);
@@ -188,6 +188,33 @@ class Users extends Controller{
 
             // Load view
             $this->view('users/login', $data);
+        }
+    }
+
+
+    public function createUserSession($user){
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['user_email'] = $user->email;
+        $_SESSION['user_name'] = $user->name;
+        redirect('/pages/index');
+    }
+
+
+    public function logout(){
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_email']);
+        unset($_SESSION['user_name']);
+        session_destroy();
+        redirect('/users/login');
+
+    }
+
+
+    public function isLoggedIn(){
+        if(isset($_SESSION['user_id'])){
+            return true;
+        }else{
+            return false;
         }
     }
 }
