2389e953bc3f4d6537c503c85a1fea5c29bd1ff6
Mr-Maccoy <134474843+Mr-Maccoy@users.noreply.github.com>
2025-01-09 22:58:52 -0600

    Post access
    Controller, Post Model and view, post form

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -196,7 +196,7 @@ class Users extends Controller{
         $_SESSION['user_id'] = $user->id;
         $_SESSION['user_email'] = $user->email;
         $_SESSION['user_name'] = $user->name;
-        redirect('/pages/index');
+        redirect('/posts');
     }
 
 
@@ -210,11 +210,5 @@ class Users extends Controller{
     }
 
 
-    public function isLoggedIn(){
-        if(isset($_SESSION['user_id'])){
-            return true;
-        }else{
-            return false;
-        }
+    
     }
-}
